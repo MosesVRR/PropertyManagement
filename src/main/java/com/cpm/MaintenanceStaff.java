1a7fd55e5720cbfb638d657e0121d82b4f181455
Moses Rupak Raj Vedala <mosesvrr@gmail.com>
2024-03-17 20:59:25 -0400

    implement OCL 24

--- src/main/java/com/cpm/MaintenanceStaff.java
+++ src/main/java/com/cpm/MaintenanceStaff.java
@@ -11,12 +11,16 @@ class MaintenanceStaff extends User {
         // Implementation
     }
 
-    //Method to validate OCL 25
+    //Method to validate OCL 25 and OCL 24
     public void changeMaintenanceStatus(MaintenanceStatus newStatus, MaintenanceRequest request) {
         if (newStatus == MaintenanceStatus.COMPLETED && (request.getDescription() == null || request.getDescription().isEmpty())) {
             System.out.println("Cannot mark request as completed without a description.");
-        } else {
+        } else if(newStatus == MaintenanceStatus.PENDING && request.getAssignedTo() == null){
+            System.out.println("Cannot set status to PENDING without assigning to a staff member.");
+        } 
+        else{
             request.setStatus(newStatus);
+            System.out.println("Set status succesfully");
             // Other necessary operations (e.g., updating database, notifying staff, etc.)
         }
     }
